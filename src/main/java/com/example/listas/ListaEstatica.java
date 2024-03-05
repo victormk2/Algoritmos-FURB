ea36cd4c7e788c8033db9319fae25ab13a6de1a8
victormk2 <victormatheusk@gmail.com>
2024-03-04 21:10:16 -0300

    Initial Commit

---
+++ src/main/java/com/example/listas/ListaEstatica.java
@@ -0,0 +1,65 @@
+package com.example.listas;
+
+public class ListaEstatica {
+
+    int[] info;
+    int tamanho;
+
+    public ListaEstatica(){
+        info = new int[10];
+        tamanho = 0;
+    }
+
+    public void inserir(int valor){
+        if (tamanho == info.length){
+            redimensionar();
+        }
+        info[tamanho] = valor;
+        tamanho++;
+    }
+
+    private void redimensionar(){
+        int[] novaInfo = new int[info.length + 10];
+
+        for (int i = 0; i < info.length; i++){
+            novaInfo[i] = info[i];
+        }
+
+        info = novaInfo;
+    }
+
+    public int buscar(int valor){
+        int position = -1;
+        
+        for (int i = 0; i< tamanho; i++){
+            if (info[i] == valor){
+                position = i;
+            }
+        }
+
+        return position;
+    }
+
+    public void remover(int valor){
+        int position = buscar(valor);
+
+        if (position > -1){
+            for(int i=position; i< tamanho -1; i++){
+                info[i] = info[i+1];
+            }
+
+            tamanho--;
+        }
+    }
+
+    public void exibir(){
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println(info[i]);
+        }
+    }
+
+    public boolean estaVazia(){
+        return tamanho==0;
+    }
+
+}
---
+++ src/test/java/com/example/listas/ListaEstaticaTest.java
@@ -0,0 +1,38 @@
+package com.example.listas;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListaEstaticaTest {
+
+    ListaEstatica listaEstatica;
+
+    @Before
+    public void setUp(){
+        listaEstatica = new ListaEstatica();
+    }
+
+    @Test
+    public void inserir(){
+        listaEstatica.inserir(10);
+
+        Assertions.assertThat(listaEstatica.info[0]).isEqualTo(10);
+    }
+
+    @Test
+    public void redimensionar(){
+        for(int i = 0; i < 10; i++) {
+            listaEstatica.inserir(i);
+        }
+
+        Assertions.assertThat(listaEstatica.info.length).isEqualTo(10);
+        Assertions.assertThat(listaEstatica.tamanho).isEqualTo(10);        
+
+        listaEstatica.inserir(10);
+
+        Assertions.assertThat(listaEstatica.info.length).isEqualTo(20);
+        Assertions.assertThat(listaEstatica.tamanho).isEqualTo(11);        
+    }
+}
